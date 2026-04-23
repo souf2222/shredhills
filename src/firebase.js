e3367ec8758d0adbc382da6269fe7faec76f18ac
Soufiane Benrqiq <soufiane@upawa.com>
2026-04-22 21:09:36 -0400

    Add Docker build support with Firebase env vars

--- src/firebase.js
+++ src/firebase.js
@@ -19,12 +19,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAgmn-1BSdCrfR2eU2p8C5k2hg6K-o0eP8",
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "shredhills-5dc27.firebaseapp.com",
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID || "shredhills-5dc27",
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "shredhills-5dc27.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "402341934400",
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID || "1:402341934400:web:43b2e90eb176db2df936a5"
 };
 
 const app     = initializeApp(firebaseConfig);
